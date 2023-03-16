e25628cd0fbb742e7b945a568619b981797a284e
nilankar19 <nilankar19@gmail.com>
2023-03-16 10:33:52 +0530

    first commit

---
+++ components/Step.tsx
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+// import { FormContext } from "../pages/explore";
+// import BasicInformation from "./Step1";
+// import Success from "./Success";
+// import Step2 from "./Step2";
+// import Step3 from "./Step3";
+// // import { FormContextType } from "../pages/explore";
+
+// export function Step() {
+//   const { activeStepIndex } = useContext<FormContextType | null>(FormContext);
+//   let stepContent;
+//   switch (activeStepIndex) {
+//     case 0:
+//       stepContent = <BasicInformation />;
+//       break;
+//     case 1:
+//       stepContent = <Step2 />;
+//       break;
+//     case 2:
+//       stepContent = <Step3 />;
+//       break;
+//     default:
+//       break;
+//   }
+
+//   return stepContent;
+// }
+
+// export default Step;
---
+++ pages/form.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+// import Stepper from "../components/Stepper";
+// import StepperControl from "..//StepperControl";
+// // import { UseContextProvider } from "./Form/contexts/StepperContext";
+
+// // import Account from "./Form/steps/Account";
+// // import Details from "./Form/steps/Details";
+// // import Payment from "./Form/steps/Payment";
+// // import Final from "./Form/steps/Final";
+
+// function Form() {
+//   const [currentStep, setCurrentStep] = useState(1);
+
+//   const steps = [
+//     "Account Information",
+//     "Personal Details",
+//     "Payment",
+//     "Complete",
+//   ];
+
+
+
+//   return (
+//     <div className="mx-auto rounded-2xl bg-white pb-2 shadow-xl md:w-1/2">
+//       {/* Stepper */}
+//       <div className="horizontal container mt-5 ">
+//         <Stepper steps={steps} currentStep={currentStep} />
+
+//         {/* <div className="my-10 p-10 ">
+//           <UseContextProvider>{displayStep(currentStep)}</UseContextProvider>
+//         </div> */}
+//       </div>
+
+//       {/* navigation button */}
+//       {currentStep !== steps.length && (
+//         <StepperControl
+//           handleClick={handleClick}
+//           currentStep={currentStep}
+//           steps={steps}
+//         />
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Form;
